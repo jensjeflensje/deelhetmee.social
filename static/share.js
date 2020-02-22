14bfcd416727b5b08d29d57d4e3cff34a609605c
jensjeflensje <jensgamesnl@gmail.com>
2020-02-22 11:48:03 +0100

    share button fix

--- static/share.js
+++ static/share.js
@@ -11,13 +11,22 @@ function share(sound_id) {
 }
 
 
-
-window.onresize = () => {
-    const mq = window.matchMedia( "(min-width: 960px)" );
+function checkShareButton() {
+    let mq = window.matchMedia( "(min-width: 960px)" );
 
     if (mq.matches) {
         document.getElementById("share-btn").style.display = "none";
     } else {
         document.getElementById("share-btn").style.display = "block";
     }
+}
+
+
+
+window.onresize = () => {
+    checkShareButton();
+}
+
+window.onload = () => {
+    checkShareButton();
 }
