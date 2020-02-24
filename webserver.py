864a45457716447a59789adaac43d91a37095ef3
lokerhp <44868937+lokerhp@users.noreply.github.com>
2020-02-24 20:53:09 +0100

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -39,6 +39,18 @@ def api_create_nos():
     sound.export(f"./static/sounds/{file_name}.mp3", format="mp3")
     return str(file_name)
 
+@app.route("/create/netflix", methods=["POST"])
+def api_create_netflix():
+    file_name = random.randint(1, 1000000)
+    temp_path = f"./temp_sounds/{file_name}.wav"
+    f = open(temp_path, "wb")
+    f.write(request.data)
+    f.close()
+    sound = audio_helper.create_sound(temp_path, prefix="sounds/netflix_prefix.wav")
+    sound.export(f"./static/sounds/{file_name}.mp3", format="mp3")
+    return str(file_name)
+
+
 if config.DEBUG:
     # Static Files
     @app.route("/static/<path:path>")
